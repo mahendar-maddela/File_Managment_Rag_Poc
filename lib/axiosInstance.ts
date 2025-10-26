e3a0200d008df45521080c5d559ec06b9c2a36fa
mahendar-maddela <mahendar1241@gmail.com>
2025-10-26 13:02:21 +0530

    updated

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -3,8 +3,7 @@ import axios from "axios";
 
 // Create instance
 const axiosApiInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1/", // your backend URL
-    // baseURL: process.env.NEXT_PUBLIC_API_URL || "https://ragpocfastapiserver-production.up.railway.app/api/v1/",
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://localhost:8000/api/v1/", // your backend URL
     withCredentials: true,
 });
 
